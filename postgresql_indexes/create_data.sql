44a3129d37403e6b025ecdb930bf467420e09d41
ivan.vakhrushev <ivvakhrushev@yandex-team.ru>
2019-03-28 12:49:49 +0300

    Added drop index

--- postgresql_indexes/create_data.sql
+++ postgresql_indexes/create_data.sql
@@ -9,5 +9,6 @@ select case when (g.idx % 2 = 0) then null else lpad(g.idx::text, 20, '0') end
 from generate_series(1, 1000000) as g (idx);
 														 
 select * from test order by id asc limit 100;
-														 
-create index concurrently idx_ref_without_nulls on test (ref) where ref is not null;
+					
+drop index concurrently if exists idx_ref_without_nulls;					
+create index concurrently if not exists idx_ref_without_nulls on test (ref) where ref is not null;
